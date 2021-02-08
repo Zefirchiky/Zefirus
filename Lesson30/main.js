307225f442b73b7035d357c8f8ee2930b27638b2
Zefirchiky <mudruk.arte@gmail.com>
2021-02-08 17:44:06 +0200

    b

---
+++ Lesson30/main.js
@@ -0,0 +1,15 @@
+// let x = 2
+//     y = 4
+//     z = 5
+//     c = x + y + z
+
+let myName = "Artem"
+    myAge = 14
+    userName = prompt("What your name?")
+
+console.log (userName)
+
+alert ("Hello " + userName)
+alert (userName + " #1")
+userAge = prompt("How old are you?")
+alert ("My age is 14 years old " + userAge) 
