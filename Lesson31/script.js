04b569b3ae92a9d393c40fa5c52cd092b2755b0c
Pyatar <legoboy.kr2005@gmail.com>
2021-02-12 16:57:32 +0200

    sdafn

---
+++ Lesson31/script.js
@@ -0,0 +1,34 @@
+let name = prompt ("What your name?")
+console.log (name)
+alert ("Hello " + name)
+
+userAnswer1 = prompt ("How open a brawser console?")
+console.log (userAnswer1)
+
+userAnswer2 = prompt ("How to make a folder with console?")
+console.log (userAnswer2)
+
+userAnswer3 = prompt ("How open code with console?")
+console.log (userAnswer3)
+
+userAnswer4 = prompt ("Which tag makes a paragraph")
+console.log (userAnswer4)
+
+console.log (prompt ("What tag creates a list item?"))
+
+console.log (prompt ("Which console command clones the repository?"))
+
+console.log (prompt ("Which console command commits the added files to GitHub?"))
+
+console.log (prompt ("Which css rule changes the background7"))
+
+console.log (prompt ("What css rule indents from the edges of the div?"))
+
+console.log (prompt ("How to make this window?"))
+
+console.log (prompt ("How to set a variable to JS?"))
+
+console.log (prompt ("What's your favorite programming language?"))
+
+console.log (prompt ("Why do you want to be a programmer?"))
+alert ("Ok, your application has been sent for evaluation!")
