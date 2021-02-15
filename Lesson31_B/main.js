2c9a97fe5fbf5041669666f37450f1cd796b81d1
Zefirchiky <mudruk.arte@gmail.com>
2021-02-15 17:46:42 +0200

    jui

---
+++ Lesson31_B/main.js
@@ -0,0 +1,17 @@
+let x = Number (prompt ("x"))
+    y = Number (prompt ("y"))
+    z = x + y
+
+alert ("Summa = " + z)
+x++
+y--
+console.log (y)
+console.log (x)
+console.log (x+y)
+console.log (y-x)
+console.log (y*x)
+console.log (y/x)
+console.log (y**x)
+console.log (x%y)
+console.log (x++)
+console.log (x<y)
